6530e7f3c2e6b8a393d696bacea1596b7cf8bda4
EskanderBejaoui <eskander.bejaoui@outlook.com>
2021-03-05 23:23:02 +0100

    Upload

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+mkdir -p /home/user/.config/spotify
+chown -R user:user /home/user/.config/spotify
+ln -s /usr/share/novnc/vnc.html /usr/share/novnc/index.html
+
+# Update Spotify
+echo "======== Update ========"
+echo "Checking for updates ..."
+sudo apt update
+sudo apt install --yes spotify-client
+echo "Done."
+
+# Create virtual display
+echo "======== Virtual display ========"
+echo "Starting display server ..."
+Xvfb :99 -screen 0 1024x768x16 &
+export DISPLAY=:99
+x11vnc -display :99 -nopw -forever -quiet -geometry 1024x768 &
+websockify -D --web=/usr/share/novnc/ 8080 localhost:5900 &
+echo "Done."
+
+# Start PulseAudio
+echo "======== PulseAudio ========"
+echo "Starting audio server ..."
+sudo -H -u user bash -c pulseaudio &>/dev/null &
+echo "Done."
+
+# Start PulseAudio
+echo "======== Spotify Client ========"
+echo "Starting Spotify ..."
+sudo -H -u user bash -c spotify --disable-gpu &>/dev/null
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DEVICE_NAME=Spotify-on-Docker
+HOST_CONFIG_DIR=/home/$USER/.config/spotify
+
+docker run -it --rm \
+  -p 8080:8080 \
+  -v $HOST_CONFIG_DIR:/home/user/.config/spotify \
+  -v $XDG_RUNTIME_DIR/pulse:/run/user/1000/pulse \
+  -h $DEVICE_NAME \
+  spotify-on-docker:latest
