2b58cb0f401bb26b6d73afa90237039376c0d5f1
Eskander Bejaoui <mail@eskander.tn>
2021-03-06 17:02:50 +0100

    Make scripts podman aware

--- run.sh
+++ run.sh
@@ -5,7 +5,15 @@ HOST_CONFIG_DIR=/home/$USER/.config/spotify
 
 mkdir -p $HOST_CONFIG_DIR
 
-docker run -it --rm \
+container=docker
+
+# If podman detected
+if [ -f "/usr/bin/podman" ]; then
+    echo "Podman detected"
+    container=podman
+fi
+
+$container run -it --rm \
   -p 8080:8080 \
   -v $HOST_CONFIG_DIR:/home/user/.config/spotify \
   -v $XDG_RUNTIME_DIR/pulse:/run/user/1000/pulse \
