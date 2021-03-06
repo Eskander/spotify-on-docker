dd7d822a13bb0d1148aca48fdeaafe2e62083cdf
Eskander Bejaoui <mail@eskander.tn>
2021-03-06 16:48:13 +0100

    Fix directory not found

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 DEVICE_NAME=Spotify-on-Docker
 HOST_CONFIG_DIR=/home/$USER/.config/spotify
 
+mkdir -p $HOST_CONFIG_DIR
+
 docker run -it --rm \
   -p 8080:8080 \
   -v $HOST_CONFIG_DIR:/home/user/.config/spotify \
