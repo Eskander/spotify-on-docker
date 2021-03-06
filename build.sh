e1c433bba7ddfa81a525c3988f6a7aeb63a58bc5
EskanderBejaoui <eskander.bejaoui@outlook.com>
2021-03-06 17:02:50 +0100

    Make scripts podman aware

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-docker build -t "spotify-on-docker:latest" .
+container=docker
+
+# If podman detected
+if [ -f "/usr/bin/podman" ]; then
+    echo "Podman detected"
+    container=podman
+fi
+
+$container build -t "spotify-on-docker:latest" .
